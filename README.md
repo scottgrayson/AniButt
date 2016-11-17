2bd02658372f281fccfb59fcabb50db850d540bc
Scott Grayson <dscottgrayson@gmail.com>
2016-11-16 22:49:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#AniButt
+Animated buttons
+
+uses:
+- vue
+- bootstrap 4
+- font awesome
+- animate.css
---
+++ anibutt.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="addClass" class="btn">
+        <span>{{content}}</span>
+        <span v-show="loading"><i class="fa fa-spin fa-spinner"></i></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            addClass: {
+                default: 'btn-secondary'
+            },
+            loading: {
+                type: Boolean,
+                default: false
+            },
+            content: {
+                default: 'content'
+            }
+        }
+    }
+</script>
+
